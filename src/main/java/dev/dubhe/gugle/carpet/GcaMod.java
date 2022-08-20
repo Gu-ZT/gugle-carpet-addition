bffe53f9abe88d7350ee5d63f7ec287746e9f401
Gu_ZT <administration@ztxy666.cn>
2022-08-20 12:22:46 +0800

    init

---
+++ src/main/java/dev/dubhe/gugle/carpet/GcaMod.java
@@ -0,0 +1,23 @@
+package dev.dubhe.gugle.carpet;
+
+import carpet.CarpetExtension;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+
+public class GcaMod implements CarpetExtension, ModInitializer, ClientModInitializer {
+
+    @Override
+    public void onGameStarted(){
+
+    }
+
+    @Override
+    public void onInitialize() {
+
+    }
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/dev/dubhe/gugle/carpet/GcaSetting.java
@@ -0,0 +1,5 @@
+package dev.dubhe.gugle.carpet;
+
+public class GcaSetting {
+
+}
