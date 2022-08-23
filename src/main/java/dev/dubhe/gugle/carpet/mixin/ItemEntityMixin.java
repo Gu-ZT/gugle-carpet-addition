e7e10e0957574cec88795d7a00bd180693cb7dfd
Gu_ZT <administration@ztxy666.cn>
2022-08-23 18:54:19 +0800

    Update 2.0.1
    ## Fixed BUG
    * Button items can be sucked away by hopper minecart, etc

--- src/main/java/dev/dubhe/gugle/carpet/mixin/ItemEntityMixin.java
+++ src/main/java/dev/dubhe/gugle/carpet/mixin/ItemEntityMixin.java
@@ -6,19 +6,32 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ItemEntity.class)
 public class ItemEntityMixin {
 
     ItemEntity self = (ItemEntity) (Object) this;
 
-    @Inject(method = "tick",at=@At("HEAD"))
-    private void tick(CallbackInfo ci){
-        ItemStack selfItem = self.getItem();
+    @Inject(method = "tick", at = @At("HEAD"))
+    private void tick(CallbackInfo ci) {
+        ItemStack selfItem = self.getEntityData().get(ItemEntity.DATA_ITEM);
         if (selfItem.getTag() != null) {
             if (selfItem.getTag().get("GcaClear") != null) {
                 if (selfItem.getTag().getBoolean("GcaClear")) {
                     self.kill();
+                }
+            }
+        }
+    }
+
+    @Inject(method = "getItem", at = @At("HEAD"), cancellable = true)
+    private void getItem(CallbackInfoReturnable<ItemStack> cir) {
+        ItemStack selfItem = self.getEntityData().get(ItemEntity.DATA_ITEM);
+        if (selfItem.getTag() != null) {
+            if (selfItem.getTag().get("GcaClear") != null) {
+                if (selfItem.getTag().getBoolean("GcaClear")) {
+                    cir.setReturnValue(ItemStack.EMPTY);
                 }
             }
         }
