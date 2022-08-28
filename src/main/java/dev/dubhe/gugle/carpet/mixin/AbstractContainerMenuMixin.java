38d63c6ca3d324521e69283062e12d9eaac341d3
Gu_ZT <administration@ztxy666.cn>
2022-08-28 18:31:15 +0800

    Update AbstractContainerMenuMixin.java

--- src/main/java/dev/dubhe/gugle/carpet/mixin/AbstractContainerMenuMixin.java
+++ src/main/java/dev/dubhe/gugle/carpet/mixin/AbstractContainerMenuMixin.java
@@ -1,14 +1,11 @@
 package dev.dubhe.gugle.carpet.mixin;
 
-import net.minecraft.core.NonNullList;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ClickType;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -16,14 +13,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(AbstractContainerMenu.class)
 public class AbstractContainerMenuMixin {
 
-    @Shadow
-    @Final
-    public NonNullList<Slot> slots;
+    AbstractContainerMenu self = (AbstractContainerMenu)(Object)this;
 
     @Inject(method = "doClick", at = @At("HEAD"), cancellable = true)
     private void doClick(int mouseX, int mouseY, ClickType clickType, Player player, CallbackInfo ci) {
         if (mouseX < 0) return;
-        Slot slot = this.slots.get(mouseX);
+        Slot slot = self.getSlot(mouseX);
         ItemStack itemStack = slot.getItem();
         if (itemStack.getTag() != null) {
             if (itemStack.getTag().get("GcaClear") != null) {
