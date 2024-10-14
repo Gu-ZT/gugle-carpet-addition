fb8b658a6efa099be37603c1ba67942cf86ace09
Gugle <administration@ztxy666.cn>
2024-10-15 05:55:03 +0800

    添加特性：假人名称前缀&假人名称后缀

--- src/main/java/dev/dubhe/gugle/carpet/GcaSetting.java
+++ src/main/java/dev/dubhe/gugle/carpet/GcaSetting.java
@@ -6,64 +6,87 @@ public class GcaSetting {
 
     public static final String GCA = "GCA";
     public static final String EXPERIMENTAL = "experimental";
+    public static final String BOT = "BOT";
 
     // 允许玩家打开假人背包
     @Rule(
-            categories = {GCA, EXPERIMENTAL}
+        categories = {GCA, EXPERIMENTAL, BOT}
     )
     public static boolean openFakePlayerInventory = false;
 
     // 允许玩家打开假人末影箱
     @Rule(
-            categories = {GCA, EXPERIMENTAL}
+        categories = {GCA, EXPERIMENTAL, BOT}
     )
     public static boolean openFakePlayerEnderChest = false;
 
     // 退出存档时保留假人
     @Rule(
-            categories = {GCA, EXPERIMENTAL}
+        categories = {GCA, EXPERIMENTAL, BOT}
     )
     public static boolean fakePlayerResident = false;
 
     // 退出存档时保留假人动作
     @Rule(
-            categories = {GCA, EXPERIMENTAL}
+        categories = {GCA, EXPERIMENTAL, BOT}
     )
     public static boolean fakePlayerReloadAction = false;
 
     // 让假人自动补货
     @Rule(
-            categories = {GCA, EXPERIMENTAL}
+        categories = {GCA, EXPERIMENTAL, BOT}
     )
     public static boolean fakePlayerAutoReplenishment = false;
 
     // 让假人自动钓鱼
     @Rule(
-            categories = {GCA, EXPERIMENTAL}
+        categories = {GCA, EXPERIMENTAL, BOT}
     )
     public static boolean fakePlayerAutoFish = false;
 
     // 让假人自动切换快损坏的工具
     @Rule(
-            categories = {GCA, EXPERIMENTAL}
+        categories = {GCA, EXPERIMENTAL, BOT}
     )
     public static boolean fakePlayerAutoReplaceTool = false;
 
+    public static final String fakePlayerNoneName = "#none";
+
+    // 假人名称前缀
+    @Rule(
+        options = {fakePlayerNoneName, "bot_"},
+        categories = {GCA, EXPERIMENTAL, BOT}
+    )
+    public static String fakePlayerPrefixName = fakePlayerNoneName;
+
+    // 假人名称后缀
+    @Rule(
+        options = {fakePlayerNoneName, "_fake"},
+        categories = {GCA, EXPERIMENTAL, BOT}
+    )
+    public static String fakePlayerSuffixName = fakePlayerNoneName;
+
+//    // 方便快捷的假人管理菜单
+//    @Rule(
+//            categories = {GCA, EXPERIMENTAL, BOT}
+//    )
+//    public static boolean commandBot = false;
+
     // 让放置的栅栏门与你点击的栅栏门拥有相同的方块状态
     @Rule(
-            categories = {GCA, EXPERIMENTAL}
+        categories = {GCA, EXPERIMENTAL}
     )
     public static boolean betterFenceGatePlacement = false;
 
     // 仅允许名称中包含“去皮”的斧头对原木去皮
     @Rule(
-            categories = {GCA, EXPERIMENTAL}
+        categories = {GCA, EXPERIMENTAL}
     )
     public static boolean betterWoodStrip = false;
 
     // 右键告示牌时与之附着的方块产生交互
     @Rule(
-            categories = {GCA, EXPERIMENTAL}
+        categories = {GCA, EXPERIMENTAL}
     )
     public static boolean betterSignInteraction = false;
 
@@ -75,13 +98,7 @@ public class GcaSetting {
 
     // 快速合成时在物品栏保留一份物品
     @Rule(
-            categories = {GCA, EXPERIMENTAL}
+        categories = {GCA, EXPERIMENTAL}
     )
     public static boolean betterQuickCrafting = false;
-
-//    // 方便快捷的假人管理菜单
-//    @Rule(
-//            categories = {GCA, EXPERIMENTAL}
-//    )
-//    public static boolean botManagement = false;
 }
---
+++ src/main/java/dev/dubhe/gugle/carpet/mixin/PlayerCommandMixin.java
@@ -0,0 +1,29 @@
+package dev.dubhe.gugle.carpet.mixin;
+
+import carpet.commands.PlayerCommand;
+import dev.dubhe.gugle.carpet.GcaSetting;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+import java.util.Locale;
+
+@Mixin(value = PlayerCommand.class, remap = false)
+abstract class PlayerCommandMixin {
+    @ModifyVariable(method = "spawn", at = @At("STORE"), remap = false)
+    private static String spawn(String value) {
+        if (
+            !GcaSetting.fakePlayerNoneName.equals(GcaSetting.fakePlayerPrefixName)
+                && !value.toLowerCase(Locale.ROOT).startsWith(GcaSetting.fakePlayerPrefixName.toLowerCase(Locale.ROOT))
+        ) {
+            value = GcaSetting.fakePlayerPrefixName + value;
+        }
+        if (
+            !GcaSetting.fakePlayerNoneName.equals(GcaSetting.fakePlayerSuffixName)
+                && !value.toLowerCase(Locale.ROOT).endsWith(GcaSetting.fakePlayerSuffixName.toLowerCase(Locale.ROOT))
+        ) {
+            value = value + GcaSetting.fakePlayerSuffixName;
+        }
+        return value;
+    }
+}
