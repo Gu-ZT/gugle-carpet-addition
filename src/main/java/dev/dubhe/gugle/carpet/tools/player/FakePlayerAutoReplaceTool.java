f4c4e32fe9b33ce871865968f0c423af430ce7d3
fcsailboat <3286637847@qq.com>
2025-05-02 20:40:32 +0800

    调整假人自动切换工具逻辑

--- src/main/java/dev/dubhe/gugle/carpet/tools/player/FakePlayerAutoReplaceTool.java
+++ src/main/java/dev/dubhe/gugle/carpet/tools/player/FakePlayerAutoReplaceTool.java
@@ -1,32 +1,131 @@
 package dev.dubhe.gugle.carpet.tools.player;
 
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.function.Predicate;
+//#if MC>=12005
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.world.item.enchantment.EnchantmentEffectComponents;
+import net.minecraft.world.item.enchantment.ItemEnchantments;
+import net.minecraft.world.item.enchantment.Enchantment;
+import it.unimi.dsi.fastutil.objects.Object2IntMap;
+import net.minecraft.core.Holder;
+
+//#else
+//$$ import net.minecraft.world.item.enchantment.EnchantmentHelper;
+//$$ import net.minecraft.world.item.enchantment.Enchantments;
+//$$ import java.util.Optional;
+//#endif
 public class FakePlayerAutoReplaceTool {
 
-    public static void autoReplaceTool(@NotNull Player fakePlayer) {
-        ItemStack mainHand = fakePlayer.getMainHandItem();
-        ItemStack offHand = fakePlayer.getOffhandItem();
-        if (!mainHand.isEmpty() && (mainHand.getMaxDamage() - mainHand.getDamageValue()) <= 10)
-            replaceTool(EquipmentSlot.MAINHAND, fakePlayer);
-        if (!offHand.isEmpty() && (offHand.getMaxDamage() - offHand.getDamageValue()) <= 10)
-            replaceTool(EquipmentSlot.OFFHAND, fakePlayer);
+    @SuppressWarnings("UnnecessaryReturnStatement")
+    //#if MC>=12100
+    public static void autoReplaceTool(@NotNull Player fakePlayer, Item item, EquipmentSlot equipmentSlot) {
+        ItemStack itemStack = fakePlayer.getItemBySlot(equipmentSlot);
+        //#else
+        //$$ public static void autoReplaceTool(@NotNull Player fakePlayer, Item item, ItemStack itemStack) {
+        //#endif
+        Predicate<ItemStack> isDamageItem = itemDamagePredicate();
+        if (isDamageItem.test(itemStack)) {
+            //#if MC>=12100
+            //#else
+            //$$ Optional<EquipmentSlot> optional = getEquipmentSlot(fakePlayer, itemStack);
+            //$$ if (optional.isEmpty()) {
+            //$$     return;
+            //$$ }
+            //$$ EquipmentSlot equipmentSlot = optional.get();
+            //#endif
+            Predicate<ItemStack> predicate = itemReplacePredicate(item);
+            boolean replaced = replaceTool(equipmentSlot, predicate, fakePlayer);
+            if (replaced) {
+                return;
+            }
+            // 没有可供切换的工具，切换到无耐久的物品以避免经验修补工具损坏
+            // - 左键挖掘方块时切换到无耐久物品通常不会有什么影响
+            // - 但是右键交互方块时（例如使用锄头锄地）切换物品可能会出现问题，例如乱放置方块或乱使用物品
+            // - 或许可以在没有合适的工具时自动停止右键？
+            // replaceTool(equipmentSlot, stack -> !stack.isDamageableItem(), fakePlayer);
+        }
     }
 
-    public static void replaceTool(EquipmentSlot slot, @NotNull Player fakePlayer) {
-        ItemStack itemStack = fakePlayer.getItemBySlot(slot);
-        for (int i = 0; i < 36; i++) {
-            ItemStack itemStack1 = fakePlayer.getInventory().getItem(i);
-            if (itemStack1.isEmpty() || itemStack1 == itemStack) continue;
-            if (itemStack1.getItem().getClass() == itemStack.getItem().getClass() && (itemStack1.getMaxDamage() - itemStack1.getDamageValue()) > 10) {
-                ItemStack itemStack2 = itemStack1.copy();
-                fakePlayer.getInventory().setItem(i, itemStack);
-                fakePlayer.setItemSlot(slot, itemStack2);
-                break;
+    //#if MC<12100
+    //$$ private static Optional<EquipmentSlot> getEquipmentSlot(@NotNull Player fakePlayer, ItemStack itemStack) {
+    //$$     for (EquipmentSlot equipmentSlot : EquipmentSlot.values()) {
+    //$$         if (fakePlayer.getItemBySlot(equipmentSlot) == itemStack) {
+    //$$             return Optional.of(equipmentSlot);
+    //$$         }
+    //$$     }
+    //$$     return Optional.empty();
+    //$$ }
+    //#endif
+
+    public static boolean replaceTool(EquipmentSlot slot, Predicate<ItemStack> predicate, @NotNull Player fakePlayer) {
+        if (predicate.test(fakePlayer.getItemBySlot(slot))) {
+            return true;
+        }
+        Inventory inventory = fakePlayer.getInventory();
+        for (int i = 0; i < inventory.getContainerSize(); i++) {
+            ItemStack itemStack = inventory.getItem(i);
+            if (itemStack.isEmpty()) {
+                continue;
+            }
+            if (predicate.test(itemStack)) {
+                ItemStack itemBySlot = fakePlayer.getItemBySlot(slot);
+                ItemStack copy = itemStack.copy();
+                inventory.setItem(i, itemBySlot);
+                fakePlayer.setItemSlot(slot, copy);
+                return true;
             }
         }
+        return false;
+    }
+
+    private static Predicate<ItemStack> itemDamagePredicate() {
+        return itemStack -> {
+            if (itemStack.isEmpty()) {
+                return true;
+            }
+            if (itemStack.isDamageableItem()) {
+                // 有经验修补的物品，保留10点耐久
+                if (hasMending(itemStack)) {
+                    int remaining = itemStack.getMaxDamage() - itemStack.getDamageValue();
+                    return remaining <= 10;
+                }
+                return false;
+            }
+            return false;
+        };
+    }
+
+    private static Predicate<ItemStack> itemReplacePredicate(Item item) {
+        return itemStack -> {
+            if (itemStack.getItem().getClass() == item.getClass()) {
+                if (hasMending(itemStack)) {
+                    return itemStack.getMaxDamage() - itemStack.getDamageValue() > 10;
+                }
+                return true;
+            }
+            return false;
+        };
+    }
+
+    private static boolean hasMending(ItemStack itemStack) {
+        //#if MC>=12100
+        ItemEnchantments enchantments = itemStack.getOrDefault(DataComponents.ENCHANTMENTS, ItemEnchantments.EMPTY);
+        for (Object2IntMap.Entry<Holder<Enchantment>> entry : enchantments.entrySet()) {
+            Holder<Enchantment> key = entry.getKey();
+            if (key.value().effects().has(EnchantmentEffectComponents.REPAIR_WITH_XP)) {
+                return true;
+            }
+        }
+        return false;
+        //#else
+        //$$ return EnchantmentHelper.getItemEnchantmentLevel(Enchantments.MENDING, itemStack) > 0;
+        //#endif
     }
 }
