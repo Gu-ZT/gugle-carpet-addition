1e7e507187396b5281bc57a6464d47f4c7dbe6f7
Cjsah <cjsah@cjsah.net>
2026-03-17 02:29:24 +0800

    feat(bot): fix bot group add/remove

--- src/main/java/dev/dubhe/gugle/carpet/commands/BotCommand.java
+++ src/main/java/dev/dubhe/gugle/carpet/commands/BotCommand.java
@@ -39,6 +39,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 import org.jetbrains.annotations.Nullable;
 
@@ -243,12 +244,12 @@ public class BotCommand {
         GroupNode group = getGroupNode(context, groupName);
         if (group == null) return 0;
         CommandSourceStack source = context.getSource();
-        List<String> bots = group.group.bots();
+        List<String> bots = new ArrayList<>(group.group.bots());
         if (!bots.remove(botName)) {
             source.sendFailure(Component.literal("Bot %s is not found in the %s.".formatted(botName, groupName)));
             return 0;
         }
-        BOT_GROUP_CONFIG.update(group.group);
+        BOT_GROUP_CONFIG.update(new BotGroupInfo(groupName, bots));
         source.sendSuccess(() -> Component.literal("Bot %s is removed from %s successfully.".formatted(botName, groupName)), false);
         return Command.SINGLE_SUCCESS;
     }
@@ -259,13 +260,13 @@ public class BotCommand {
         GroupNode group = getGroupNode(context, groupName);
         if (group == null) return 0;
         CommandSourceStack source = context.getSource();
-        List<String> bots = group.group.bots();
+        List<String> bots = new ArrayList<>(group.group.bots());
         if (bots.contains(botName)) {
             source.sendFailure(Component.literal("Bot %s is already added.".formatted(botName)));
             return 0;
         }
         bots.add(botName);
-        BOT_GROUP_CONFIG.update(group.group);
+        BOT_GROUP_CONFIG.update(new BotGroupInfo(groupName, bots));
         source.sendSuccess(() -> Component.literal("Bot %s is added to %s successfully.".formatted(botName, groupName)), false);
         return Command.SINGLE_SUCCESS;
     }
